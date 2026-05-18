373ed59d7166277ccfb16ca3bb0970e5bd845b8c
Saharier Omi <gsaharier761@gmail.com>
2026-05-19 01:05:37 +0600

    add package.json with initial dependencies and scripts

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+const port = process.env.PORT || 5000;
+
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const db = client.db("studynookdb");
+    const roomsCollection = db.collection("rooms");
+
+    app.get("/rooms", async (req, res) => {
+      const cursor = await roomsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/rooms/:roomsId", async (req, res) => {
+      const { roomsId } = req.params;
+      const result = await roomsCollection.findOne({
+        _id: new ObjectId(roomsId),
+      });
+      res.send(result);
+    });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Server is running! Welcome to Study Nook.");
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
