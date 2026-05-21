4ea86a162bc4507efb2604ecb59ed543073de0cd
Saharier Omi <gsaharier761@gmail.com>
2026-05-21 21:06:03 +0600

    add my listing api

--- index.js
+++ index.js
@@ -33,19 +33,19 @@ async function run() {
       res.send(result);
     });
 
-   app.get("/rooms/:id", async (req, res) => {
-     const { id } = req.params;
-     const result = await roomsCollection.findOne({
-       _id: new ObjectId(id),
-     });
+    app.get("/rooms/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await roomsCollection.findOne({
+        _id: new ObjectId(id),
+      });
 
-     const bookingCount = await bookingCollection.countDocuments({
-       roomId: id,
-       status: "confirmed",
-     });
+      const bookingCount = await bookingCollection.countDocuments({
+        roomId: id,
+        status: "confirmed",
+      });
 
-     res.send({ ...result, bookingCount });
-   });
+      res.send({ ...result, bookingCount });
+    });
 
     app.get("/featured-rooms", async (req, res) => {
       const cursor = await roomsCollection.find().limit(6);
@@ -132,6 +132,14 @@ async function run() {
         { $set: { status: "cancelled" } },
       );
 
+      res.send(result);
+    });
+
+    app.get("/my-listings", async (req, res) => {
+      const { userId } = req.query;
+      const result = await roomsCollection
+        .find({ "owner.id": userId })
+        .toArray();
       res.send(result);
     });
 
