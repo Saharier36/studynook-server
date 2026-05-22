ec416a59869f0447feb28b799398615331f46589
Saharier Omi <gsaharier761@gmail.com>
2026-05-22 20:38:47 +0600

    add vercel.json

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ const verifyToken = async (req, res, next) => {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const db = client.db("studynookdb");
     const roomsCollection = db.collection("rooms");
     const bookingCollection = db.collection("bookings");
